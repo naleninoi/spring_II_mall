d6387f40f380de45f71149b07fb42ad9a1eb8ef8
Viktor Kochetov <naleninoi@gmail.com>
2021-11-14 16:21:48 +0300

    Lesson 3 - AOP

---
+++ src/main/java/ru/gb/java1154/config/FlowAspectConfiguration.java
@@ -0,0 +1,9 @@
+package ru.gb.java1154.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Configuration
+@EnableAspectJAutoProxy
+public class FlowAspectConfiguration {
+}
