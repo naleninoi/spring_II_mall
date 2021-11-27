09d696a35a250ff2457e8d4e4c0e8b5162baeb0e
Viktor Kochetov <naleninoi@gmail.com>
2021-11-27 12:13:36 +0300

    Lesson 5-6 / add Web page & MVC controller to list products

--- src/main/java/ru/gb/java1154/controller/ProductController.java
+++ src/main/java/ru/gb/java1154/controller/ProductController.java
@@ -4,13 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import ru.gb.java1154.entity.Product;
+import ru.gb.java1154.render.ProductR;
+import ru.gb.java1154.service.ProductService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping(value = "/products")
 public class ProductController {
 
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("")
     public String showProducts(Model model) {
+        List<Product> products = productService.findAllProducts();
+        List<ProductR> result = products.stream().map(ProductR::new).collect(Collectors.toList());
+        model.addAttribute("products", result);
         return "products";
     }
 }
